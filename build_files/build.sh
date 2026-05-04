533b3c9a0de38ba9bb41c12d8bbc7cdb8e351f2f
Kate Scarlet <sbt3357@gmail.com>
2026-05-04 10:52:51 +0800

    chore(build): 更新安装脚本

--- build_files/build.sh
+++ build_files/build.sh
@@ -6,10 +6,15 @@ dnf5  --setopt=install_weak_deps=False install -y \
     android-tools bcc bpftop bpftrace ccache git-subtree git-lfs \
     rclone \
     waypipe \
+    iotop \
+    flatpak-builder \
     papirus-icon-theme \
     ibus-rime librime-lua \
-    jetbrains-mono-fonts \
-    podman-docker qemu libvirt qemu-kvm virt-manager incus containerd
+    jetbrains-mono-fonts-all \
+    podman-docker containerd \
+    qemu qemu-kvm libvirt libvirt-nss virt-manager edk2-ovmf guestfs-tools \
+    cockpit-machines cockpit-ostree \
+    incus incus-agent
 
 dnf5 -y copr enable scottames/ghostty
 dnf5 -y install ghostty
